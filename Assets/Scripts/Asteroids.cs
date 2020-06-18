44e45c3f9177e1aa9a9f8f72b0d15a0b938a48f9
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2020-06-18 16:08:23 +0700

    upload files

---
+++ Assets/Scripts/Asteroids.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroids : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-speed, 0);
+    }
+}
---
+++ Assets/Scripts/SpawnAsteroids.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroids : MonoBehaviour
+{
+    public GameObject asteroidsPrefab;
+    public float respawnTime = 5f;
+    public Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(asteroidsWave());
+    }
+
+    private void spawnAsteroids() {
+        GameObject a = Instantiate(asteroidsPrefab) as GameObject;
+        a.transform.position = new Vector2(screenBounds.x + 1, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+    IEnumerator asteroidsWave() {
+        while(true) {
+            yield return new WaitForSeconds(respawnTime);
+            spawnAsteroids();
+        }
+    }
+}
